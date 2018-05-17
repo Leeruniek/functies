4bb1c776a1899079df9c197c3603cdd6851b0b26
Andrei <andrei@codemachiner.com>
2018-05-17 11:58:59 +0200

    Add "core__deep-equal" function

--- src/core__equals/equals.js
+++
@@ -1,9 +0,0 @@
-/**
- * Check if a is equal to b (strict equality)
- *
- * @param  {mixed}   a  First value
- * @param  {mixed}   b  Second value
- *
- * @return  {boolean}
- */
-module.exports = a => b => a === b
