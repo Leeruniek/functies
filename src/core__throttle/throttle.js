4c4ef06b488e71c14bb1d6037d93bab8db9a2875
Andrei <andrei@codemachiner.com>
2018-06-06 14:14:07 +0200

    Add "core__throttle" for running function once every x ms

---
+++ src/core__throttle/throttle.js
@@ -0,0 +1,32 @@
+/**
+ * Call a function only if it hasn't been called in the last `timeWindow` ms.
+ *
+ * @param  {function} fn            Function to be ran
+ * @param  {integer}  timeWindow    Time between each `fn` call
+ *
+ * @return {function}               Either return `fn` if you've passed the
+ *                                  `timeWindow` or return a timer that will
+ *                                  run the `fn` in `timeWindow` ms
+ */
+module.exports = ( fn, { timeWindow = 50, bind = null } = {} ) => {
+  let lastExecution = new Date( ( new Date() ).getTime() - timeWindow )
+  let finalRunTimer
+
+  return ( ... args ) => {
+    const hasTimeWindowPassed = ( lastExecution.getTime() + timeWindow ) <= ( new Date() ).getTime()
+
+    if ( hasTimeWindowPassed ) {
+      lastExecution = new Date()
+
+      fn.apply( bind, args )
+    }
+
+    // reset timer at every function call
+    clearTimeout( finalRunTimer )
+
+    finalRunTimer = setTimeout( () => {
+      lastExecution = new Date()
+      fn.apply( bind, args )
+    }, timeWindow )
+  }
+}
