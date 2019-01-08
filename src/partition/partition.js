e265e606c51aef3450a6bb762739d5813d7c8aac
Tomas Bravo <tomas@leeruniek.nl>
2019-01-08 16:14:20 +0100

    Add docs and a custom docs theme, use external type declaration for partition

--- src/partition/partition.js
+++ src/partition/partition.js
@@ -1,28 +1,47 @@
 // @flow
 
+declare function partition<V, T: $ReadOnlyArray<V>>(
+  fn: (x: V) => boolean
+): T => [T, T]
+
+type AccType<T> = {|
+  pass: $ReadOnlyArray<T>,
+  fail: $ReadOnlyArray<T>,
+|}
+
 /**
- * partition splits a list based on a predicate function.
+ * Split a list based on a predicate function.
  *
- * @sig (fn: (T: boolean)) => (input: T[]): [T[], T[]]
+ * @signature
+ * <V, T: $ReadOnlyArray<T>>(fn: (x: V) => boolean): T => [T, T]
  *
- * **`(fn: (T: boolean)) => (input: T[]): [T[], T[]]`**
+ * @param fn
+ * A predicate function.
+ *
+ * @example
+ * partition(x => x % 2 === 0)([1,2,3,4,5]) = [[2,4],[1,3,5]]
+ *
+ * @returns
+ * A curried function taking a value of type `T`,
+ * returning a two-tuple (list) of `T`s - the first element consists
+ * of elements for which the predicate returned `true`, the second
+ * of elements for which it returned `false`.
  */
-module.exports = function partition<T>(
-  fn: (x: T) => boolean
-): (T[]) => [T[], T[]] {
-  return (input: T[]): [T[], T[]] => {
-    const result = input.reduce(
-      (acc, val) => {
-        if (fn(val)) {
-          acc.pass = [...acc.pass, val]
-        }
+export default function partition<V, T: $ReadOnlyArray<V>>(
+  fn: (x: V) => boolean
+): T => [T, T] {
+  return (input: T): [T, T] => {
+    const startingAcc: AccType<V> = { pass: [], fail: [] }
 
-        acc.fail = [...acc.fail, val]
+    const result = input.reduce((acc, val) => {
+      if (fn(val)) {
+        acc.pass = [...acc.pass, val]
+      }
 
-        return acc
-      },
-      { pass: [], fail: [] }
-    )
+      acc.fail = [...acc.fail, val]
+
+      return acc
+    }, startingAcc)
 
     return [result.pass, result.fail]
   }
