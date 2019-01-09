5034fc75d7badd1025b5d7f5c2613be3655d75bd
Tomas Bravo <tomas@leeruniek.nl>
2019-01-09 14:36:05 +0100

    Separate typedefs from implementation for partition using a .js.flow file

--- src/partition/partition.js
+++ src/partition/partition.js
@@ -1,48 +1,33 @@
 // @flow
 
-declare function partition<V, T: $ReadOnlyArray<V>>(
-  fn: (x: V) => boolean
-): T => [T, T]
-
-type AccType<T> = {|
-  pass: $ReadOnlyArray<T>,
-  fail: $ReadOnlyArray<T>,
-|}
+import type { PartitionType } from "./partition.js.flow"
 
 /**
  * Split a list based on a predicate function.
  *
  * @signature
- * <V, T: $ReadOnlyArray<T>>(fn: (x: V) => boolean): T => [T, T]
+ * <A>(fn: (x: A) => boolean) => (input: A[]) => [A[], A[]]
  *
  * @param fn
  * A predicate function.
  *
  * @return
- * A curried function taking a value of type `T`,
- * returning a two-tuple (list) of `T`s - the first element consists
+ * A curried function taking a value of type `A`,
+ * returning a two-tuple of `A`s - the first element consists
  * of elements for which the predicate returned `true`, the second
  * of elements for which it returned `false`.
  *
  * @example
  * partition(x => x % 2 === 0)([1,2,3,4,5]) = [[2,4],[1,3,5]]
  */
-export default function partition<V, T: $ReadOnlyArray<V>>(
-  fn: (x: V) => boolean
-): T => [T, T] {
-  return (input: T): [T, T] => {
-    const startingAcc: AccType<V> = { pass: [], fail: [] }
+const partition: PartitionType = <A>(fn) => input =>
+  input.reduce(
+    (acc, val) => {
+      const [pass, fail] = acc
 
-    const result = input.reduce((acc, val) => {
-      if (fn(val)) {
-        acc.pass = [...acc.pass, val]
-      }
+      return fn(val) ? [[...pass, val], fail] : [pass, [...fail, val]]
+    },
+    [[], []]
+  )
 
-      acc.fail = [...acc.fail, val]
-
-      return acc
-    }, startingAcc)
-
-    return [result.pass, result.fail]
-  }
-}
+export { partition }
