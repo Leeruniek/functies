dab5e35d68601f8a2393dd70eb805735c8ea0fff
Tomas Bravo <tomas@leeruniek.nl>
2019-01-11 15:16:13 +0100

    Use a recursive type to type curry, export curry with the module and adjust the docs styling

--- src/curry/curry.js
+++ src/curry/curry.js
@@ -8,7 +8,9 @@ import type { CurryType } from "./curry.js.flow"
  * Partially apply a function.
  *
  * @signature
- * <A>(fn: Function) => (args: mixed[]) => A
+ * <A>(fn: (...args: mixed[]) => A) => CurryReturnType<A>
+ *
+ * where CurryReturnType<A> = (...mixed[]) => A | CurryReturnType<A>
  *
  * @param fn
  * The function to apply.
@@ -19,7 +21,8 @@ import type { CurryType } from "./curry.js.flow"
  * @return
  * If the number of arguments provided is sufficient to call the function,
  * call the function and return the result. Otherwise, return a new function
- * which takes additional parameters, returning the result of calling `curry` on the function with the original parameters and the additional parameters.
+ * which takes additional parameters, returning the result of calling `curry`
+ * on the function with the provided parameters.
  *
  * @example
  * curry((a, b) => a + b)(1)(2) = 3
