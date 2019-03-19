647bec23230b089e22eab1d30750f1b789873cf8
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-03-19 15:12:48 +0100

    Add tests and docs for `allBy`

--- src/all/all.js
+++ src/all/all.js
@@ -9,9 +9,9 @@ import type { AllType } from "./all.js.flow"
  *
  * @name  all
  * @param {Function}  fn      Function that all elements need to satisfy
- * @param {Array}     source  Source array
+ * @param {Array}     source  Input array
  *
- * @return {boolean}
+ * @return {boolean} True if all objects satisfy, false otherwise
  *
  * @tag Core
  * @signature (fn: Function) => (source: Array): boolean
@@ -19,12 +19,37 @@ import type { AllType } from "./all.js.flow"
  * @example
  * all(isNumber)([1, 2, 3])
  * // => true
+ *
  * all(is)([1, "asd", null])
  * // => false
  */
 const all: AllType = <A>(fn) => source =>
   (Array.isArray(source) ? source : [source]).every(fn)
 
+export { all }
+
+/**
+ * Test if all object properties match any of the input array
+ * Test if one object's properties match all objects in input array
+ *
+ * @name   allBy
+ * @param  {Object}  subset  Set of properties that should match
+ * @param  {Array}   source  Input array
+ *
+ * @return {boolean} True if all objects match, false otherwise
+ *
+ * @tag Core
+ * @signature (subset: Object) => (source: Object[]): boolean
+ *
+ * @example
+ * all({
+ *   id: isString
+ * })([
+ *   { id: "uuid1", name: "foo"}
+ *   { id: "uuid2", name: "bar"}
+ * ])
+ * // => true
+ */
 const allBy = byMatch(all)
 
-export { all, allBy }
+export { allBy }
--- src/all/all.test.js
+++ src/all/all.test.js
@@ -1,5 +1,5 @@
 import test from "tape"
-import { all } from ".."
+import { all, allBy } from ".."
 
 const isNumber = source => Number.isFinite(source)
 
@@ -13,6 +13,22 @@ test("core::all", t => {
   )
 
   t.equal(all(isNumber)(2), true, "Check non array input")
+
+  t.equal(
+    allBy({
+      id: isNumber,
+    })([{ id: 1 }, { id: 2 }]),
+    true,
+    "Array of objects should have all items match given subset"
+  )
+
+  t.equal(
+    allBy({
+      "!id": isNumber,
+    })([{ id: "1" }, { id: 2 }]),
+    false,
+    "Array of objects should not have all items match given subset"
+  )
 
   t.end()
 })
