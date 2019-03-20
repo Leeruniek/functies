2439ec971f213bf9c88b77f1300d3e0eae3756e2
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-03-20 16:06:51 +0100

    Update docstring `findIndexBy`

--- src/find-index-by/find-index-by.js
+++ src/find-index-by/find-index-by.js
@@ -1,14 +1,14 @@
 import { isMatch } from "../is-match/is-match"
-import { curry } from ".."
+import { curry } from "../curry/curry"
 
 /**
- * Find the position of first element that matches the filter criteria
+ * Find the position of first element that matches the subset criteria
  *
  * @name       findIndexBy
  * @tag        Array
- * @signature  (filter: Object) => (source: Object[]): number
+ * @signature  (subset: Object) => (source: Object[]): number
  *
- * @param  {Object}    filter  Object containing one or more fields
+ * @param  {Object}    subset  Object containing one or more fields
  * @param  {Object[]}  source  Source input
  *
  * @return {number}
@@ -17,20 +17,19 @@ import { curry } from ".."
  * const comments = [{id: 1, body: ""}, {id: 2, body: "dolor"}]
  *
  * findIndexBy({id: 2})(comments)
- * // 1
+ * // => 1
+ *
  * findIndexBy({id: 3})(comments)
- * // -1
+ * // => -1
  */
-function findIndexBy(filter, source) {
+const findIndexBy = curry((subset, source) => {
   for (let i = 0, length = source.length - 1; i <= length; i++) {
-    if (isMatch(filter)(source[i])) {
+    if (isMatch(subset)(source[i])) {
       return i
     }
   }
 
   return -1
-}
+})
 
-const curried = curry(findIndexBy)
-
-export { curried as findIndexBy }
+export { findIndexBy }
--- src/find-index-by/find-index-by.test.js
+++ src/find-index-by/find-index-by.test.js
@@ -1,26 +1,6 @@
 import test from "tape"
 import { findIndexBy } from ".."
 
-/**
- * Find the position of first element that matches the filter criteria
- *
- * @name       findIndexBy
- * @tag        Array
- * @signature  (filter: Object) => (source: Object[]): number
- *
- * @param  {Object}    filter  Object containing one or more fields
- * @param  {Object[]}  source  Source input
- *
- * @return {number}
- *
- * @example
- * const comments = [{id: 1, body: ""}, {id: 2, body: "dolor"}]
- *
- * findIndexBy({id: 2})(comments)
- * // 1
- * findIndexBy({id: 3})(comments)
- * // -1
- */
 test("array::findIndexBy", t => {
   const comments = [{ id: 1, body: "" }, { id: 2, body: "dolor" }]
 
